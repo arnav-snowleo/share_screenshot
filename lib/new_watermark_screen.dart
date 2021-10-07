4bd0fa080644a725862ff2ea0d5060da509d7d6b
arnav-snowleo <prakashsinghaarav@gmail.com>
2021-10-07 23:25:46 +0530

    comments removed

--- lib/new_watermark_screen.dart
+++ lib/new_watermark_screen.dart
@@ -22,7 +22,6 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
   File? _capturedImage;
   File? _watermarkImage;
   Uint8List? _watermarkedImage;
-  // File? _watermarkedImage;
   Uint8List? img;
 
   File? _getCapturedImgFile;
@@ -37,21 +36,18 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
   Widget build(BuildContext context) {
     Future shareIt(Uint8List bytes) async {
       final directory = await getApplicationDocumentsDirectory();
-
       final image = File('${directory.path}/waterMarked.png');
       image.writeAsBytesSync(bytes);
-
       final text = 'Download the app!';
       await Share.shareFiles([image.path], text: text);
     }
 
     Future<String> getFilePath() async {
       // Directory appDocumentsDirectory =
-      //     await getApplicationDocumentsDirectory(); // 1
-      Directory? appDocumentsDirectory =
-          await getExternalStorageDirectory(); // 1
-      String appDocumentsPath = appDocumentsDirectory!.path; // 2
-      String filePath = '$appDocumentsPath/foodss.png'; // 3
+      //     await getApplicationDocumentsDirectory();
+      Directory? appDocumentsDirectory = await getExternalStorageDirectory();
+      String appDocumentsPath = appDocumentsDirectory!.path;
+      String filePath = '$appDocumentsPath/foodss.png';
       return filePath;
     }
 
@@ -63,11 +59,10 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
 
     void saveFile() async {
       await [Permission.storage].request();
-
-      File file = File(await getFilePath()); // 1
+      File file = File(await getFilePath());
       final byteData = await rootBundle.load('assets/images/foodss.png');
       file.writeAsBytes(byteData.buffer
-          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes)); // 2
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
     }
 
     Future<File> getCapturedImage(Uint8List bytes) async {
@@ -100,7 +95,6 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
                     img = image;
                   });
                   print('captured');
-                  // await saveAndShare(image);
                 }
                 await getFilePath();
                 saveFile();
@@ -115,41 +109,31 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
                 }
                 _getWatermarkImgFile =
                     await getWatermarkImageFromFilesDirectory();
+                _getCapturedImgFile = await getCapturedImage(img!);
 
                 setState(() {
                   gotAppIcon = _getWatermarkImgFile;
-                });
-                print('watermark img got and setstate');
-                _getCapturedImgFile = await getCapturedImage(img!);
-                setState(() {
                   gotCaptured = _getCapturedImgFile;
                 });
-                print('captured img got and setstate');
 
                 if (gotCaptured == null || gotAppIcon == null) {
                   return;
                 }
-                //start watermark process
                 ui.Image? originalImage =
                     ui.decodeImage(gotCaptured!.readAsBytesSync());
                 ui.Image? watermarkImage =
                     ui.decodeImage(gotAppIcon!.readAsBytesSync());
 
-                // add watermark over originalImage
-                // initialize width and height of watermark image
                 ui.Image? image = ui.Image(50, 50);
                 ui.drawImage(image, watermarkImage!);
 
-                // give position to watermark over image
                 ui.copyInto(originalImage!, image,
                     dstX: originalImage.width - 50 - 25,
                     dstY: originalImage.height - 50 - 25);
 
-                // for adding text over image (x,y)
                 ui.drawString(originalImage, ui.arial_48, 75,
                     originalImage.height - 50 - 25, 'try our app now');
 
-                // Store the watermarked image to a File
                 List<int> wmImage = ui.encodePng(originalImage);
                 setState(() {
                   _watermarkedImage = Uint8List.fromList(wmImage);
@@ -157,7 +141,6 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
                 if (_watermarkedImage != null) {
                   await shareIt(_watermarkedImage!);
                 }
-                //end
               },
               child: Text('watermark and share'),
             ),
@@ -169,9 +152,5 @@ class _NewWatermarkScreenState extends State<NewWatermarkScreen> {
 }
 
 // Skipped 111 frames!  The application may be doing too much work on its main thread.
-//error-> Unhandled Exception: Null check operator used on a null value
-//ref->
-//https://stackoverflow.com/questions/64278595/null-check-operator-used-on-a-null-value
 
-// good read for image processing
-// https://github.com/brendan-duncan/image/wiki/Examples
+
